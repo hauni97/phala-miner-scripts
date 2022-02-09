b16630828d4963372a3eb7ef5f2306d1836eb2d2
David Haunschild <37558304+hauni97@users.noreply.github.com>
2022-02-09 12:24:49 +0100

    Update status-API-WS-fix0222.sh

--- status-API-WS-fix0222.sh
+++ status-API-WS-fix0222.sh
@@ -7,8 +7,13 @@ set -a
 
 # This deletes and replaces the status.sh file with an updated status.sh script
 
-cd ..
-rm /opt/phala/scripts/status.sh
-wget https://raw.githubusercontent.com/Phala-Network/solo-mining-scripts/improvement-test/scripts/en/status.sh
-mv status.sh /opt/phala/scripts/
-exit 1
+function update_phala_status()
+{
+  cd ..
+  rm /opt/phala/scripts/status.sh
+  wget https://raw.githubusercontent.com/Phala-Network/solo-mining-scripts/improvement-test/scripts/en/status.sh
+  mv status.sh /opt/phala/scripts/
+  exit 1
+}
+
+update_phala_status
