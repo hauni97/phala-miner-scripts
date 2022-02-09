d8d9cc5d38e1fa006c7195af7b88fff40757c41b
David Haunschild <davidh99@hotmail.de>
2022-02-09 11:05:39 +0100

    removed function

--- status-API-WS-fix0222.sh
+++ status-API-WS-fix0222.sh
@@ -3,14 +3,9 @@
 set -a 
 
 # This deletes and replaces the status.sh file with an updated status.sh script
-function status_fix()
-{
-    cd ..
-    rm /opt/phala/scripts/phala.sh
-    wget https://raw.githubusercontent.com/Phala-Network/solo-mining-scripts/improvement-test/scripts/en/status.sh
-    mv status.sh /opt/phala/scripts/
 
-fi
-}
-
-status_fix()
+cd ..
+rm /opt/phala/scripts/phala.sh
+wget https://raw.githubusercontent.com/Phala-Network/solo-mining-scripts/improvement-test/scripts/en/status.sh
+mv status.sh /opt/phala/scripts/
+exit 1
