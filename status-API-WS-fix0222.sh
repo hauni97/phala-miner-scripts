5f38d854332934947eab4161f868db515db95908
David Haunschild <37558304+hauni97@users.noreply.github.com>
2022-02-10 11:24:07 +0100

    rm -rf parameter

--- status-API-WS-fix0222.sh
+++ status-API-WS-fix0222.sh
@@ -10,7 +10,7 @@ set -a
 function update_phala_status()
 {
   cd ..
-  rm /opt/phala/scripts/status.sh
+  rm -rf /opt/phala/scripts/status.sh
   wget https://raw.githubusercontent.com/Phala-Network/solo-mining-scripts/improvement-test/scripts/en/status.sh
   mv status.sh /opt/phala/scripts/
   exit 1
